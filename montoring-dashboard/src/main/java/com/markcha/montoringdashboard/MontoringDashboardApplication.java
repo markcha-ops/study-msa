fd93615a8f60d63a175a523de03da833e15f0f25
markcha <21thpoem@naver.com>
2022-01-03 18:00:40 +0900

    b

---
+++ montoring-dashboard/src/main/java/com/markcha/montoringdashboard/MontoringDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.markcha.montoringdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontoringDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontoringDashboardApplication.class, args);
+	}
+
+}
